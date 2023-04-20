5b98c32cc57bd6fd2634b0d457e151036f18edd4
Paul Faass <paul.faass@pxio.de>
2023-04-20 16:47:29 +0200

    feat(storyblok): complete the storyblok getting started tutorial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,23 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Feature from '../components/Feature';
+import Grid from '../components/Grid';
+import Teaser from '../components/Teaser';
+import Page from '../components/Page';
+
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+};
+
+storyblokInit({
+  accessToken: process.env.STORYBLOK_API_TOKEN,
+  use: [apiPlugin],
+  components
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
