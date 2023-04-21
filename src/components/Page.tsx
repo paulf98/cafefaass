bc073ae9ccd8d4f11010ba744be30095dea1f52c
Paul Faass <paul.faass@pxio.de>
2023-04-21 12:30:55 +0200

    fix(page): check if blok is there and display loading if not

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,11 +1,16 @@
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
  
-const Page = ({ blok }: any) => (
-  <main className="py-4" {...storyblokEditable(blok)}>
-    {blok.body.map((nestedBlok: any) => (
-      <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-      ))}
-</main>
-);
+const Page = ({ blok }: any) => {
+  if (!blok || !blok.body) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <main className="py-4" {...storyblokEditable(blok)}>
+      {blok.body.map((nestedBlok: any) => (
+        <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+        ))}
+    </main>
+  )
+};
  
 export default Page;
