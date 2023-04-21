a7e5052f9384efa073e3dd7e7053fdfb700be544
Paul Faass <paul.faass@pxio.de>
2023-04-21 12:36:11 +0200

    fix(ui): small component adjustments

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,9 +2,11 @@ import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
  
 const Grid = ({ blok }: any) => {
   return (
-    <div className="grid grid-cols-3" {...storyblokEditable(blok)}>
+    <div className="grid grid-cols-3 " {...storyblokEditable(blok)}>
     {blok.columns.map((nestedBlok: any) => (
-      <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+      <div key={nestedBlok._uid} className="flex justify-center">
+        <StoryblokComponent blok={nestedBlok} />
+      </div>
     ))}
 </div>
   );
