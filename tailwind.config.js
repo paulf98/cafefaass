d7464d76422badda5cc803c9e4b28e31dcd3269f
Paul Faass <paul.faass@pxio.de>
2023-04-21 12:06:59 +0200

    chore(theme): change the primary color and button text color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {},
+  daisyui: {
+    themes: [
+      {
+          pastel: {
+          ...require('daisyui/src/colors/themes')['[data-theme=pastel]'],
+          primary: '#D74178',
+          '.btn-primary': {
+            'color': 'white',
+          },
+        },
+      }
+    ],
+  },
   plugins: [    
     require('@tailwindcss/typography'),
     require("daisyui")
