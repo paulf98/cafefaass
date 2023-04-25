ab91ac0fdf98259a2d18faeba3e20fd5f5ecd093
Paul Faass <paul.faass@pxio.de>
2023-04-25 19:14:47 +0200

    fix(carousel): change carousel to responsive image container

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,15 +3,17 @@ import Image from "next/image";
 
 const Carousel = ({ blok }: any) => {
   return (
-    <div className="carousel rounded-box mx-auto prose my-4 carousel-center p-4 space-x-4 bg-pink-100" {...storyblokEditable(blok)}>
+    <div className="flex justify-center gap-8 flex-wrap sm:flex-nowrap w-full rounded-box mx-auto prose my-8 " {...storyblokEditable(blok)}>
       {blok.images.map((image: any, index: number) => {
-        return <div className="carousel-item relative w-52 h-40" key={index}>
-          <Image src={image.filename} alt={image.alt}
-            fill={true}
-            className="m-0 rounded-box object-fill"
-            loading="lazy" 
-          />
-        </div>
+        return <Image 
+          key={index}
+          src={image.filename} 
+          alt={image.alt}
+          className="m-0 rounded object-fit"
+          loading="lazy" 
+          width={225}
+          height={160}
+        />
       })
       }
     </div>
