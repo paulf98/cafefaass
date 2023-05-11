071aafb77e7075627e3b5372962d271f0dc1df86
Paul Faass <paul.faass@pxio.de>
2023-05-11 17:18:42 +0200

    fix(video): adjust width for small screen sizes

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -2,7 +2,7 @@ import { storyblokEditable } from '@storyblok/react'
 
 export default function Video({blok}: any) {
   return (
-    <div className='w-4/6 mx-auto py-4' {...storyblokEditable(blok)}>
+    <div className='w-full md:w-4/6 mx-auto py-4' {...storyblokEditable(blok)}>
       <iframe src={blok.link.url} className='w-full h-80 rounded-md shadow-md hover:shadow-xl'>
       </iframe>
     </div>
