1ff4583c69297b5c15452950d7f412d4ebe3c210
Paul Faass <paul.faass@pxio.de>
2023-08-30 11:13:18 +0200

    fix(text): text now has an alignment option field

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,7 +1,7 @@
 import { storyblokEditable, renderRichText } from "@storyblok/react";
- 
+
 const Text = ({ blok }: any) => {
-  return <div className="my-8 prose mx-auto" {...storyblokEditable(blok)} dangerouslySetInnerHTML={
+  return <div className={["my-8 prose mx-auto md:text-xl", blok.alignment].join(' ')} {...storyblokEditable(blok)} dangerouslySetInnerHTML={
     { __html: renderRichText(blok.text) }
   }>
   </div>;
