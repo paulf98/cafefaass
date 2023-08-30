59d573490adbc76f03c15ddfcca3e9668bdf35ca
Paul Faass <paul.faass@pxio.de>
2023-08-30 11:13:50 +0200

    fix(news): heading is consistently styled here too

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,10 +1,26 @@
+import { useEffect } from 'react';
 import ArticleTeaser from './ArticleTeaser';
 import { storyblokEditable } from '@storyblok/react';
+import { Kalam } from 'next/font/google';
+import { annotate } from 'rough-notation';
+
+const headlineFont = Kalam({ subsets: ['latin'], weight: '700' });
 
 const News = ({ blok }: any) => {
+
+  useEffect(() => {
+    const e = document.getElementById(blok._uid);
+    const annotation = annotate(e as HTMLElement, { type: 'underline', color: '#1EAE9B' });
+    annotation.show();
+  }, []);
+
   return (
     <div className='py-8 mb-4 max-w-screen-xl mx-auto'>
-      <h1 className='text-center text-4xl font-bold mb-8'>{blok.title}</h1>
+      <h2 id={blok._uid}
+        className={[
+          'text-5xl mb-8 font-bold w-fit mx-auto text-primary',
+          headlineFont.className,
+        ].join(' ')}>{blok.title}</h2>
       <div
         className="w-full flex gap-6 mx-auto px-4 md:px-8 flex-wrap justify-around"
         {...storyblokEditable(blok)}
