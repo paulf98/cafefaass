17ec8eb23fd39305adbbd7f6081b2de55d2f5965
Paul Faass <paul.faass@pxio.de>
2023-09-01 16:22:04 +0200

    chore(theme): define accent color

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
             'color': 'white',
           },
           "neutral": "#D74178",
+          "accent": "#1EAE9B",
           "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
           "--rounded-btn": "0.66rem", // border radius rounded-btn utility class, used in buttons and similar element
           "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
