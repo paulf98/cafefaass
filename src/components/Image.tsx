835b5c97f4a9c70b992733f97752a43d67d47428
Paul Faass <paul.faass@pxio.de>
2023-09-01 14:32:54 +0200

    fix(image): adjust size properties

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 export default function Img({blok}: any) {
   return (
     <div className='w-4/6 max-w-prose mx-auto' {...storyblokEditable(blok)}>
-      <Image src={blok.image.filename} alt={blok.image.alt} width={320} height={180} 
-        className='rounded-md mx-auto object-contain'
+      <Image src={blok.image.filename} alt={blok.image.alt} width={480} height={180}
+        className='rounded-md mx-auto object-contain w-auto h-auto max-h-80'
       />
     </div>
   )
