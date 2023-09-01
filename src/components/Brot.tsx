6f67833d56b5cbb6dfcd8631ff00d7942ec49f7f
Paul Faass <paul.faass@pxio.de>
2023-09-01 16:22:19 +0200

    chore(brot): adjust brot component

--- src/components/Brot.tsx
+++ src/components/Brot.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
-import React from 'react'
+import { Kalam } from 'next/font/google';
+
+const headlineFont = Kalam({ subsets: ['latin'], weight: '700' });
+
 
 export default function Brot({ blok }: any) {
-  console.log(blok)
   return (
     <li className='border rounded-md flex flex-col sm:flex-row even:sm:flex-row-reverse gap-4 max-w-prose mx-auto mt-8 shadow-sm'>
       <Image src={blok.image.filename} alt={blok.image.alt} width={200} height={200} className='rounded-md w-full' />
       <div className='flex flex-col justify-around gap-4 p-4'>
-        <h1 className='text-2xl font-bold'>{blok.name}</h1>
+        <h1 className={[headlineFont.className, 'text-3xl text-primary font-bold'].join(' ')}>{blok.name}</h1>
         <p className='text-lg'>{blok.description}</p>
         <p className='text-sm italic text-gray-400'>Allergene: {blok.allergene}</p> 
       </div>
