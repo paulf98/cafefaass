726ec08941912a6895d075526d530fc5dd0733e1
Paul Faass <paul.faass@pxio.de>
2023-09-07 20:47:32 +0200

    fix(carousel): small layouting adjustments

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Carousel = ({ blok }: any) => {
   return (
     <div
-      className="prose rounded-box mx-auto my-8 flex w-full flex-wrap justify-center gap-8 sm:flex-nowrap "
+      className="prose rounded-box mx-auto my-8 flex w-full flex-nowrap justify-center md:gap-4 lg:gap-8"
       {...storyblokEditable(blok)}
     >
       {blok.images.map((image: any, index: number) => {
@@ -13,10 +13,11 @@ const Carousel = ({ blok }: any) => {
             key={index}
             src={image.filename}
             alt={image.alt}
-            className="m-0 rounded object-fill"
+            className="m-0 object-fill md:rounded"
             loading="lazy"
             width={225}
             height={160}
+            sizes="(min-width: 768px) 225px, 33vw"
           />
         );
       })}
