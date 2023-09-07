9e1483dc18af9d2d48f2e15f318ca801e879e3d2
Paul Faass <paul.faass@pxio.de>
2023-09-07 14:33:59 +0200

    feat(footer): add new links to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,24 +4,8 @@ import { RiFacebookBoxFill, RiInstagramFill } from 'react-icons/ri';
 
 export default function Footer() {
   return (
-    <footer className="footer footer-center z-40 flex flex-col bg-gradient-to-tr from-pink-900 via-primary to-pink-500 p-4 py-8 text-base-100 sm:p-10">
+    <footer className="footer footer-center z-40 flex flex-col bg-primary p-4 py-8 text-base-100 sm:p-10">
       <div className="flex w-full max-w-prose flex-col flex-wrap justify-around gap-4 text-lg md:flex-row">
-        <div className="flex flex-col items-start justify-start">
-          <Link className="link-hover link" href={'/impressum'}>
-            Impressum
-          </Link>
-          <Link className="link-hover link" href={'/datenschutz'}>
-            Datenschutz
-          </Link>
-        </div>
-        <div className="flex justify-center gap-4">
-          <Link href={'https://www.facebook.com/cafefaass'}>
-            <RiFacebookBoxFill size={48} />
-          </Link>
-          <Link href={'https://www.instagram.com/cafefaass/'}>
-            <RiInstagramFill size={48} />
-          </Link>
-        </div>
         <Image
           src="/Logo.png"
           width={128}
@@ -29,17 +13,41 @@ export default function Footer() {
           alt="Logo"
           className="rounded-xl"
         />
+        <div className="flex flex-col items-start gap-1">
+          <h4 className="mb-2 text-lg font-bold">Weitere Links</h4>
+          <Link className="link-hover link" href={'/stellenangebote'}>
+            Stellenangebote
+          </Link>
+          <Link className="link-hover link" href={'/geschichte'}>
+            Geschichte
+          </Link>
+          <Link className="link-hover link" href={'/ernaehrung-und-gesundheit'}>
+            Ernährung & Gesundheit
+          </Link>
+        </div>
+        <div className="flex flex-col items-start justify-start">
+          <Link className="link-hover link" href={'/impressum'}>
+            Impressum
+          </Link>
+          <Link className="link-hover link" href={'/datenschutz'}>
+            Datenschutz
+          </Link>
+          <div className="mt-4 flex justify-center gap-4">
+            <Link href={'https://www.facebook.com/cafefaass'}>
+              <RiFacebookBoxFill size={48} />
+            </Link>
+            <Link href={'https://www.instagram.com/cafefaass/'}>
+              <RiInstagramFill size={48} />
+            </Link>
+          </div>
+        </div>
       </div>
       <div>
         <p className="italic">
           Copyright © {new Date().getFullYear()} - Café Faass -{' '}
           <a
             href="https://paulf.dev"
-            className="link-hover link
-					text-sm
-					italic
-					text-base-100
-					"
+            className="link-hover link text-sm italic text-base-100"
             target={'_blank'}
             rel="noreferrer"
           >
