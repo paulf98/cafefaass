607b0f8a989b381435bdd5d3023adb43c955317f
Paul Faass <paul.faass@pxio.de>
2023-09-08 12:26:38 +0200

    fix(bloks): increase image height in torte teaser

--- src/components/TorteTeaser.tsx
+++ src/components/TorteTeaser.tsx
@@ -8,15 +8,16 @@ export default function TorteTeaser({ torte }: any) {
   return (
     <div className="card w-96 bg-base-100 shadow-xl" {...storyblokEditable(torte)}>
       {torte.teaser_img.filename && (
-        <figure>
-          <Image
-            className="mb-4 w-full rounded-t-xl object-cover object-center md:h-36 lg:h-48"
-            src={torte.teaser_img.filename}
-            alt={torte.teaser_img.alt}
-            width={200}
-            height={90}
-          />
-        </figure>
+        <div className="relative mb-4 h-64">
+          <figure>
+            <Image
+              className="rounded-t-xl object-cover"
+              src={torte.teaser_img.filename}
+              alt={torte.teaser_img.alt}
+              layout="fill"
+            />
+          </figure>
+        </div>
       )}
       <div className="card-body">
         <h2 className="card-title">{torte.title}</h2>
